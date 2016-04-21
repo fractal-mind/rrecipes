91dd048f89d9a672a914d2357c247b2309b10df3
fractal-mind <williamcmoody@gmail.com>
2016-04-21 08:59:00 -0700

    Initial Commit. Sets up package.json and readme.md, changes entry point to index.js, installs react-localstorage

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ var webpack = require('webpack');
 
 
 module.exports = {
-  entry: './src/js/main.js',
+  entry: './src/js/index.js',
   output: { path: __dirname, filename: 'bundle.js'},
   module: {
     loaders: [
